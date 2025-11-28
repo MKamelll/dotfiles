b59cd2c9385299ebfbaadb1062ee0591596df75b
Mina Kamel <minallkamel@gmail.com>
2025-11-28 02:20:26 +0200

    remove mason plugin

--- nvim/lua/config/lsp/pylsp.lua
+++ nvim/lua/config/lsp/pylsp.lua
@@ -1,15 +1,15 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 vim.lsp.config("pylsp", {
-	capabilities = capabilities,
-	cmd = { "pylsp" },
-	filetypes = { "python" },
-	root_markers = {
-		"pyproject.toml",
-		"setup.py",
-		"setup.cfg",
-		"requirements.txt",
-		"Pipfile",
-		".git",
-	},
+    capabilities = capabilities,
+    cmd = { "uv", "run", "pylsp" },
+    filetypes = { "python" },
+    root_markers = {
+        "pyproject.toml",
+        "setup.py",
+        "setup.cfg",
+        "requirements.txt",
+        "Pipfile",
+        ".git",
+    },
 })
 vim.lsp.enable("pylsp")
