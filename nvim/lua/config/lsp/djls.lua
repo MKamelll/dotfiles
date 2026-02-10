ee21ff70eeacff6c99411963f829e43c62843f25
Mina Kamel <minallkamel@gmail.com>
2026-02-10 05:19:16 +0200

    i don't even know what's different

--- nvim/lua/config/lsp/djls.lua
+++ nvim/lua/config/lsp/djls.lua
@@ -1,9 +1,9 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 vim.lsp.config("djls", {
-	capabilities = capabilities,
-	cmd = { "djls", "serve" },
-	filetypes = { "htmldjango", "html", "python" },
-	root_markers = { "manage.py", "pyproject.toml", ".git" },
+    capabilities = capabilities,
+    cmd = { "djls", "serve" },
+    filetypes = { "htmldjango", "html", "python" },
+    root_markers = { "manage.py", "pyproject.toml", ".git" },
 })
 vim.lsp.enable("djls")
