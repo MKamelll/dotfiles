17fcd63c295b4151efa710c1554db543e991ce06
Mina Kamel <minallkamel@gmail.com>
2026-03-02 22:43:28 +0200

    nvim: racket

---
+++ nvim/lua/config/lsp/racket_langserver.lua
@@ -0,0 +1,18 @@
+---@brief
+---
+--- [https://github.com/jeapostrophe/racket-langserver](https://github.com/jeapostrophe/racket-langserver)
+---
+--- The Racket language server. This project seeks to use
+--- [DrRacket](https://github.com/racket/drracket)'s public API to provide
+--- functionality that mimics DrRacket's code tools as closely as possible.
+---
+--- Install via `raco`: `raco pkg install racket-langserver`
+
+---@type vim.lsp.Config
+vim.lsp.config("racket_langserver", {
+    cmd = { 'racket', '--lib', 'racket-langserver' },
+    filetypes = { 'racket', 'scheme' },
+    root_markers = { '.git' },
+})
+
+vim.lsp.enable("racket_langserver")
