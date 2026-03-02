f52b4e03296b5144ec28313e80cfaafe3b1e629e
Mina Kamel <minallkamel@gmail.com>
2026-03-03 01:28:26 +0200

    nvim: remove djls from python

--- nvim/lua/config/lsp/djls.lua
+++ nvim/lua/config/lsp/djls.lua
@@ -3,7 +3,7 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 vim.lsp.config("djls", {
     capabilities = capabilities,
     cmd = { "djls", "serve" },
-    filetypes = { "htmldjango", "html", "python" },
+    filetypes = { "htmldjango", "html" },
     root_markers = { "manage.py", "pyproject.toml", ".git" },
 })
 vim.lsp.enable("djls")
