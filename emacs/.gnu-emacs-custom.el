69ca3b5735e0e03ad7b566482d912058ad82293c
Mina Kamel <minallkamel@gmail.com>
2026-04-03 19:03:42 +0200

    fuck gcc and fuck emacs

--- emacs/.gnu-emacs-custom.el
+++ emacs/.gnu-emacs-custom.el
@@ -246,7 +246,7 @@
     :server-id 'tailwindcss-ls))
 
   ;; clangd
-  (setq lsp-clients-clangd-args '("--header-insertion=never" "--completion-style=detailed"))
+  (setq lsp-clients-clangd-args '("--header-insertion=never" "--completion-style=detailed" "--query-driver=/usr/bin/g++"))
   )
 
 ;; templ golang
@@ -512,7 +512,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
+     "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
      default))
  '(package-selected-packages nil))
 (custom-set-faces
