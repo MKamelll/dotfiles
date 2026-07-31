fc1a5046eac87c60e0a12c3add9853c61c9860c5
Mina Kamel <minallkamel@gmail.com>
2026-07-31 11:48:01 +0300

    nvim vim.pack

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,27 +1,53 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
+vim.pack.add({
+    "https://github.com/numtostr/comment.nvim",
+    "https://github.com/stevearc/conform.nvim",
+    "https://github.com/blazkowolf/gruber-darker.nvim",
+    "https://github.com/lukas-reineke/indent-blankline.nvim",
+    "https://github.com/miikanissi/modus-themes.nvim",
+    "https://github.com/jake-stewart/multicursor.nvim",
+    "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/rachartier/tiny-inline-diagnostic.nvim",
+})
+
+vim.pack.add({
+  "https://github.com/nvim-lua/plenary.nvim",
+  { src = "https://github.com/m00qek/baleia.nvim", version = "v1.3.0" },
+  { src = "https://github.com/ej-shafran/compile-mode.nvim", version = "v5.0.0" },
+})
+
+vim.g.compile_mode = {
+    input_word_completion = true,
+    baleia_setup = true,
+}
+
+vim.pack.add({
+  "https://github.com/hrsh7th/cmp-nvim-lsp",
+  "https://github.com/hrsh7th/cmp-buffer",
+  "https://github.com/hrsh7th/cmp-path",
+  "https://github.com/L3MON4D3/LuaSnip",
+  "https://github.com/saadparwaiz1/cmp_luasnip",
+  "https://github.com/rafamadriz/friendly-snippets",
+  "https://github.com/hrsh7th/nvim-cmp",
+})
+
+vim.pack.add({
+  "https://github.com/nvim-lua/plenary.nvim",
+  { src = "https://github.com/nvim-telescope/telescope.nvim", version = "v0.1.9" },
+})
+
+vim.pack.add({
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
+})
+
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(ev)
+    if ev.data.spec.name == "nvim-treesitter" and ev.data.kind ~= "delete" then
+      vim.cmd("TSUpdate")
     end
-end
-vim.opt.rtp:prepend(lazypath)
+  end,
+})
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
-require("lazy").setup({
-    spec = {
-        { import = "plugins" },
-    },
-    checker = { enabled = true },
-})
 
 require("config")
